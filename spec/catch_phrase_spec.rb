66b968ebdc76fb8e1197d4135c71ae364f21dff8
Jeff Johnson <synotik@hotmail.com>
2020-11-02 02:52:56 +0000

    Automatically backed up by Learn

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -1,6 +1,4 @@
-require "spec_helper"
-
-```ruby
+ruby
 require "spec_helper"
 describe "#catch_phrase" do
   it "puts out a catch phrase" do
@@ -8,4 +6,4 @@ describe "#catch_phrase" do
     expect{catch_phrase(phrase)}.to output("It's-a me, Mario!\n").to_stdout
   end
 end
-```
+
